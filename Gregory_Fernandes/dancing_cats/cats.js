db36178040b857fa925209f5a55d246da4ebb035
reachgregpff <gregpffwdi2@gmail.com>
2015-10-06 14:09:17 +1100

    updated cats to flip image instead of using a fipped image

--- Gregory_Fernandes/dancing_cats/cats.js
+++ Gregory_Fernandes/dancing_cats/cats.js
@@ -11,7 +11,7 @@ var catDance = function(){
 
 }
 
-var direction = "forward";
+var goRight = true;
 
 var catWalk = function(){
   
@@ -20,14 +20,19 @@ var catWalk = function(){
   var currentLeft = parseInt(catImg.style.left, 10);  //converts string to integer
   //console.log("currentLeft is: " + currentLeft);
 
-  if(direction === "forward") {
+  if(goRight === true) {
     catImg.style.left = (currentLeft + movePixels) + 'px';
 
-    /*if((currentLeft > (window.innerWidth/2 - catImg.width))  &&  (currentLeft < (window.innerWidth/2 + catImg.width))){
+    //console.log(" ---o--- currentLeft ----o---- " + currentLeft);
+
+    /*if((currentLeft > (window.innerWidth/2 - catImg.width/2))  &&  (currentLeft < (window.innerWidth/2))){
     
+      //console.log(" ------ currentLeft -------- " + currentLeft);
       catImg.src = "meow_dance.gif";
-      catTimer = window.setInterval(catWalk, 2000);
-      window.clearInterval(catTimer);
+      //catTimer = window.setInterval(catWalk, 2000);
+      //window.clearInterval(catTimer);
+      //stopCatWalk();
+      //startCatWalk();
 
     } else{
       catImg.src = "meow.gif";
@@ -39,17 +44,18 @@ var catWalk = function(){
   }
   
   //console.log ("*********** " + window.innerWidth + " *********** " + catImg.width);
-  if(currentLeft > (window.innerWidth - catImg.width) && direction === "forward") {
-    catImg.style.left = window.innerWidth ;                     //reset left position of image
-    direction = "backward";                                  //change direction flag
-    catImg.src = "meow-reverse.gif";                                  // flip image
- 
+  if(currentLeft > (window.innerWidth - catImg.width) && goRight === true) {
+    //catImg.style.left = window.innerWidth ;                     //reset left position of image
+    goRight = false;                                  //change goRight flag
+    //catImg.src = "meow-reverse.gif";                                  // flip image
+    catImg.style.transform = "scaleX(-1)";
 
   } 
-  else if(currentLeft < 0 && direction === "backward") {
-    catImg.style.left = '0px';                                  //reset left position of image
-    direction = "forward";                                   //change direction flag
-    catImg.src = "meow.gif";                                   // flip image
+  else if(currentLeft < 0 && goRight === false) {
+    //catImg.style.left = '0px';                                  //reset left position of image
+    goRight = true;                                   //change goRight flag
+    //catImg.src = "meow.gif";                                   // flip image
+    catImg.style.transform = "scaleX(1)";
   }
 
 
