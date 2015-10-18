172ab807bd3846ccf4712735fb93167c3d8648b0
reachgregpff <gregpffwdi2@gmail.com>
2015-10-19 10:25:00 +1100

    answer

---
+++ Gregory_Fernandes/Oct-20-warmup-VigCipher/VigenereCipher.js
@@ -0,0 +1,24 @@
+function getInput( input) {
+      console.log("Please enter the  message " + input + ":" );
+      return prompt();
+}
+
+var message = getInput("message");
+var keyword = getInput("keyword");
+var alphabets = "abcdefghijklmnopqrstuvwxyz";
+var encryptedWord = [];
+
+function encrypt(){
+  message= message.split("");
+  keyword=keyword.split("");
+  var newKeyword = [];
+
+  for(var i =0; i<message.length; i++) {
+    newKeyword.push( keyword[i%(keyword.length)]);
+    encryptedWord.push(alphabets[(alphabets.indexOf(message[i]) + alphabets.indexOf(newKeyword[i]))%alphabets.length]);
+  }
+}
+
+encrypt();
+$('h3').html("Message: " + message.join(" ") + "<br>Keyword: " + keyword.join(" ") );
+$('h2').html("Your encrypted word is : "  + encryptedWord.join(" "));
