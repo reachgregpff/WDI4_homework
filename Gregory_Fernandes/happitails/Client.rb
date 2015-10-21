1bc6a5bcf55f6d8dfd926de422eda14e7569a27d
reachgregpff <gregpffwdi2@gmail.com>
2015-10-22 08:41:42 +1100

    deleted commented code

--- Gregory_Fernandes/happitails/Client.rb
+++ Gregory_Fernandes/happitails/Client.rb
@@ -13,7 +13,6 @@ class Client
     puts Rainbow("Client Name: #{@name}").black.bg(:yellow)
     puts "Number of children: #{@number_of_children}, age: #{@age}."
     puts Rainbow("Pets owned by this client: ").underline
-    #@pets.map { | pet | pet.display}
     @pets.map {|name, value| value.display}
     puts "\n"
   end 
@@ -21,16 +20,6 @@ class Client
   def adopt_animal pet
     @pets[pet.name] = pet
   end
-
-  #def up_for_adoption pet_name
-  #  for i in 0..(@pets.length-1)
-  #    if @pets[i].name == pet_name
-  #      pet_index = i
-  #    end
-  #  end
-  #  delete_this_pet = @pets.delete_at(pet_index)   #this returns the deleted pet
-  #  return delete_this_pet
-  #end
 
   def up_for_adoption pet_name
     @pets.delete(pet_name)   #this gets returned
